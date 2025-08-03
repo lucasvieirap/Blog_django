ac9926142f7da9ebf893254b9b6dab834c12e8a9
lucsvp <lucasvieirapalavro@gmail.com>
2025-08-03 12:56:29 -0300

    feat: post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ['created_at', 'updated_at']
+    list_display = ['title', 'author', 'created_at', 'updated_at']
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=60)
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
